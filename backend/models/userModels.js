20ae2216db4625c9abd4e8f2772ab8ac3925b694
rafikouk <rafikyamli@gmail.com>
2023-05-30 12:23:45 +0100

    test

---
+++ backend/models/userModels.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minLength: [6, "Password must be at least 6 characters"],
+        maxLength: [23, "Password must be at most 23 characters"]
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+        default: ""
+    },
+    phone: {
+        type: String,
+        default: "+216"
+    },
+    bio: {
+        type: String,
+        maxLength: [250, "bio must be at most 250 characters"],
+        default: "bio"
+    },
+
+}, {
+    timestamps: true,
+})
+
+const User = mongoose.model("User", userSchema)
+module.exports = User
